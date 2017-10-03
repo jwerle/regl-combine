e57273acd7d58a97433f770da047568f176692b1
Joseph Werle <joseph.werle@gmail.com>
2017-10-03 18:28:09 -0400

    refactor and rename

--- index.js
+++ index.js
@@ -5,86 +5,171 @@ const assert = require('assert')
 
 const noop = () => void 0
 
+const kReglOptions = '__reglOptions'
+
+// as defined here https://github.com/regl-project/regl/blob/gh-pages/regl.js#L68
+const reglContextKeys = [
+  'drawingBufferHeight',
+  'drawingBufferWidth',
+  'framebufferHeight',
+  'framebufferWidth',
+  'pixelRatio',
+  'tick',
+  'time',
+  'viewportHeight',
+  'viewportWidth',
+]
+
+/**
+ * Wraps regl constructor with a function that injects a context
+ * property exposing the options used to create the regl command.
+ * They property merges in regl options found in the injected context
+ * before injecting into scope. The constructor options are also assigned
+ * to a `.reglOptions` property on the command function.
+ *
+ * wrap(regl: Function) -> (opts: Object) => regl(opts: Object) -> void
+ */
 function wrap(regl) {
   assert('function' == typeof regl,
-         "regl-trampoline: Expecting regl constructor function.")
+         "regl-combine: Expecting regl constructor function.")
   return Object.assign(wrapRegl, regl)
   function wrapRegl(opts) {
-    assert(opts && 'object' == typeof opts,
-          "regl-trampoline: Expecting object of regl constructor options.")
-    opts = extend.command(opts)
-    opts.context = opts.context || {}
-    Object.assign(opts.context, {
-      reglOptions: ({reglOptions}) => {
-        reglOptions = extend.command(reglOptions, opts)
-        if (reglOptions.context && 'reglOptions' in reglOptions.context) {
-          delete reglOptions.context.reglOptions
-        }
-        return reglOptions
-      }
-    })
-    return regl(opts)
+    return Object.assign(regl(injectCommandOptions(opts)),
+                         {[kReglOptions]: opts})
   }
 }
 
-function compose(regl, commands) {
+/**
+ * Extends a regl command object by injecting command options
+ * into the context object as a special readonly property.
+ *
+ * injectCommandOptions(opts: Object) -> Object
+ */
+function injectCommandOptions(opts) {
+  return Object.assign(extend.command(opts), {
+    context: Object.assign({}, opts.context, Object.create(null, {
+      reglOptions: { get() { return this[kReglOptions] } },
+      [kReglOptions]: {
+        configurable: false,
+        enumerable: false,
+        writable: false,
+        value: getReglOptions
+      }
+    }))
+  })
+  function getReglOptions(ctx) {
+    const reglOptions = extend.command(ctx[kReglOptions], opts)
+    if (reglOptions.context && kReglOptions in reglOptions.context) {
+      delete reglOptions.context[kReglOptions]
+    }
+    return reglOptions
+  }
+}
+
+/**
+ * combine(regl: Function, commands: Array<Function>) -> Function
+ */
+function combine(regl, ...commands) {
+  if (Array.isArray(commands[0])) {
+    commands = commands[0]
+  }
   assert('function' == typeof regl,
-         "regl-trampoline: Expecting regl constructor function.")
+    "regl-combine: Expecting regl constructor function.")
   assert(Array.isArray(commands),
-         "regl-trampoline: Expecting array of regl commands.")
+    "regl-combine: Expecting array of regl commands.")
+
+  commands = flattenCommands(commands)
   regl = wrap(regl)
-  const getContext = regl({})
-  const opts = {}
-  let command = null
-  exec(noop)
-  return Object.assign(exec, {commands})
+
+  const getContext = createCommand()
+  const currentReglOpts = {}
+  const groups = []
+
+  let ret = null
+
+  return Object.assign(exec, {commands, groups, isCombined: true})
+
+  function flattenCommands(commands) {
+    return commands.map(select).reduce(flatten, [])
+    function select(command, i) {
+      return command && Array.isArray(command.commands)
+        ? splice(command).commands
+        : command
+    }
+
+    function flatten(commands, command) {
+      return commands.concat(command)
+    }
+
+    function splice(command) {
+      return commands.splice(commands.indexOf(command), 1)[0]
+    }
+  }
+
+  function createCommand(opts) {
+    return regl(opts || {})
+  }
+
   function exec(args, done) {
-    let ret = null
     if ('function' == typeof args) {
       done = args
-      args = {}
-    }
-    return 'function' == typeof command ? callback() : next(0).run()
-    function next(index) {
-      return index >= commands.length
-        ? trampa.wrap(callback())
-        : trampa.lazy(step)
-      function step() {
-        if ('object' == typeof commands[index]) {
-          commands[index] = regl(commands[index])
-        }
-
-        if ('function' != typeof commands[index]) {
-          ret = next(++index)
-        } else {
-          commands[index](args, () => {
-            // command[i] may be a faux regl command so we capture
-            // the regl context explicitly with regl
-            getContext(copy)
-            ret = next(++index)
-          })
-        }
-        return ret
-      }
+      args = null
     }
 
-    function copy(ctx) {
-      Object.assign(opts, extend.command(opts, ctx.reglOptions))
+    if (0 == groups.length) {
+      generate(0).run()
     }
 
-    function callback() {
-      if (null == command) { command = regl(opts) }
-      if (args && 'object' == typeof args) {
-        return command(args, done)
-      } else if ('function' == typeof done) {
-        return command(done)
+    return getContext(args, ({}, args) => loop(0, args))
+    function loop(index, args) {
+      return index < groups.length
+        ? groups[index++](args, () => loop(index, args))
+        : getContext(args, done)
+    }
+
+    function generate(index) {
+      if (index >= commands.length) {
+        concat()
+        return trampa.wrap(null)
       } else {
-        return command()
+        return trampa.lazy(step)
+      }
+      function step() {
+        const command = commands[index]
+        if ('object' == typeof command) {
+          copy(command)
+        } else if ('function' == typeof command) {
+          if (kReglOptions in command) {
+            copy(command)
+          } else {
+            concat()
+            groups.push(command)
+          }
+        }
+        return generate(++index)
       }
     }
   }
+
+  function concat() {
+    const kopts = {}
+    let klen = 0
+    for (const k in currentReglOpts) {
+      void klen ++
+      kopts[k] = currentReglOpts[k]
+      delete currentReglOpts[k]
+    }
+    return klen && groups.push(createCommand(kopts))
+  }
+
+  function copy(ctx) {
+    return Object.assign(
+      currentReglOpts,
+      extend.command(currentReglOpts, ctx[kReglOptions] || ctx))
+  }
 }
 
-module.exports = {
-  compose, wrap
-}
+module.exports = combine
+Object.assign(module.exports, {
+  combine, wrap
+})
